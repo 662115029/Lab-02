2d84b6e59f77676bba7a50987ce86fbd02997bdc
Nuthathai <inn.nuthathai@gmail.com>
2025-07-04 10:30:12 +0700

    check point 4

--- 331-intro-to-vite/src/views/HomeView.vue
+++ 331-intro-to-vite/src/views/HomeView.vue
@@ -1,9 +1,11 @@
-<script setup lang="ts">
+<!-- <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
-</script>
+</script> -->
+<script setup lang="ts"></script>
 
 <template>
   <main>
     <TheWelcome />
   </main>
+  <div class="home"></div>
 </template>
