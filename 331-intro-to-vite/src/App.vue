d4ad3cc8ccdaf5337f955d79a3c5776e813e79c0
662115029 <nuthathai_pool@cmu.ac.th>
2025-08-08 09:06:59 +0700

    Lab05 question10

--- 331-intro-to-vite/src/App.vue
+++ 331-intro-to-vite/src/App.vue
@@ -9,13 +9,13 @@ const { message } = storeToRefs(store)
 <template>
   <div class="text-center font-sans text-gray-700 antialias">
     <header>
-      <div id="flashMessage" v-if="message">
+      <div id="flashMessage" class="animate-fade" v-if="message">
         <h4>{{ message }}</h4>
       </div>
       <div class="wrapper">
-        <nav>
-          <RouterLink :to="{ name: 'event-list-view' }">Event</RouterLink> |
-          <RouterLink :to="{ name: 'about' }">About</RouterLink> |
+        <nav class="py-6">
+          <RouterLink class="font-bold text-gray-700" exact-active-class="text-green-500" :to="{ name: 'event-list-view' }">Event</RouterLink> |
+          <RouterLink class="font-bold text-gray-700" exact-active-class="text-green-500" :to="{ name: 'about' }">About</RouterLink> |
           <RouterLink :to="{ name: 'student-list-view' }">Students</RouterLink>
         </nav>
       </div>
@@ -26,7 +26,7 @@ const { message } = storeToRefs(store)
 </template>
 
 <style>
-nav {
+/* nav {
   padding: 30px;
 }
 nav a {
@@ -48,7 +48,7 @@ nav a.router-link-exact-active {
 
 h2 {
   font-size: 20px;
-}
+} */
 
 @keyframes yellowfade {
   from {
